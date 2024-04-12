fcc58687fd0fe8e75094f2186637ed8d478a1b08
winmord <winmord@yandex.ru>
2024-04-12 23:47:37 +0300

    feature: add cache usage to DbService

--- hw11-cache/build.gradle.kts
+++ hw11-cache/build.gradle.kts
@@ -1,4 +1,21 @@
 dependencies {
     implementation("ch.qos.logback:logback-classic")
     implementation("org.ehcache:ehcache")
+
+    implementation ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
+
+    implementation("org.hibernate.orm:hibernate-core")
+    implementation("org.flywaydb:flyway-core")
+
+    implementation("org.postgresql:postgresql")
+
+    testImplementation("com.h2database:h2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
 }
---
+++ hw11-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -0,0 +1,68 @@
+package ru.otus.crm.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.sessionmanager.TransactionManager;
+import ru.otus.crm.model.Client;
+
+public class DbServiceClientImpl implements DBServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
+
+    private final DataTemplate<Client> clientDataTemplate;
+    private final TransactionManager transactionManager;
+    private final HwCache<Long, Client> cache;
+
+    public DbServiceClientImpl(
+            TransactionManager transactionManager,
+            DataTemplate<Client> clientDataTemplate,
+            HwCache<Long, Client> cache) {
+        this.transactionManager = transactionManager;
+        this.clientDataTemplate = clientDataTemplate;
+        this.cache = cache;
+    }
+
+    @Override
+    public Client saveClient(Client client) {
+        return transactionManager.doInTransaction(session -> {
+            var clientCloned = client.clone();
+            if (client.getId() == null) {
+                var savedClient = clientDataTemplate.insert(session, clientCloned);
+                log.info("created client: {}", clientCloned);
+                cache.put(client.getId(), savedClient);
+                return savedClient;
+            }
+            var savedClient = clientDataTemplate.update(session, clientCloned);
+            log.info("updated client: {}", savedClient);
+            cache.remove(client.getId());
+            cache.put(client.getId(), savedClient);
+            return savedClient;
+        });
+    }
+
+    @Override
+    public Optional<Client> getClient(long id) {
+        Client clientFromCache = cache.get(id);
+        if (clientFromCache != null) {
+            return Optional.of(clientFromCache);
+        }
+
+        return transactionManager.doInReadOnlyTransaction(session -> {
+            var clientOptional = clientDataTemplate.findById(session, id);
+            log.info("client: {}", clientOptional);
+            return clientOptional;
+        });
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return transactionManager.doInReadOnlyTransaction(session -> {
+            var clientList = clientDataTemplate.findAll(session);
+            log.info("clientList:{}", clientList);
+            return clientList;
+        });
+    }
+}
