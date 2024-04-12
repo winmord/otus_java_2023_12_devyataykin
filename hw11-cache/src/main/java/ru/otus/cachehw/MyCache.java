cd2000db2da8970a4f125db52b00d9ddec21b787
winmord <winmord@yandex.ru>
2024-04-12 22:11:25 +0300

    feature: implement MyCache methods

--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,30 +1,46 @@
 package ru.otus.cachehw;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class MyCache<K, V> implements HwCache<K, V> {
-    // Надо реализовать эти методы
+    private final Map<K, V> cache = new WeakHashMap<>();
+    private final List<HwListener<K, V>> listeners = new ArrayList<>();
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.put(key, value);
+        notifyAllListeners(key, value, "ADD");
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = cache.remove(key);
+        notifyAllListeners(key, value, "DELETE");
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        V value = cache.get(key);
+        notifyAllListeners(key, value, "GET");
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.remove(listener);
+    }
+
+    private void notifyAllListeners(K key, V value, String action) {
+        for (var listener : listeners) {
+            listener.notify(key, value, action);
+        }
     }
 }
