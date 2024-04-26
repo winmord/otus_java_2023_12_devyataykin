ceac8ad1b06460cbfc7c1cde58802099d6942d09
winmord <winmord@yandex.ru>
2024-04-26 22:08:53 +0300

    fix: change configClass method storing during processing and split processing method into smaller methods

--- hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -21,47 +21,15 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
+        List<Method> methods = getMethods(configClass);
+        Object configClassInstance = getInstance(configClass);
 
-        for (Method method : configClass.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(AppComponent.class)) {
-                appComponents.add(method);
-            }
-        }
-
-        appComponents.sort((Comparator.comparingInt(
-                o -> ((Method) o).getDeclaredAnnotation(AppComponent.class).order())));
-
-        Constructor<?> constructor;
-        Object configClassInstance;
-
-        try {
-            constructor = configClass.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            configClassInstance = constructor.newInstance();
-        } catch (NoSuchMethodException
-                | InvocationTargetException
-                | InstantiationException
-                | IllegalAccessException e) {
-            throw new DiException(e.getMessage());
-        }
-
-        for (Object object : appComponents) {
-            Method method = (Method) object;
-            Object[] parameters;
-            AppComponent annotation = method.getDeclaredAnnotation(AppComponent.class);
-
-            if (!appComponentsByName.containsKey(annotation.name())) {
-                Class<?>[] parameterTypes = method.getParameterTypes();
-                if (parameterTypes.length == 0) {
-                    parameters = parameterTypes;
-                } else {
-                    parameters = new Object[parameterTypes.length];
-                    for (int i = 0; i < parameterTypes.length; i++) {
-                        parameters[i] = getAppComponent(parameterTypes[i]);
-                    }
-                }
-
-                makeBean(method, configClassInstance, annotation.name(), parameters);
+        for (Method method : methods) {
+            String annotationName =
+                    method.getDeclaredAnnotation(AppComponent.class).name();
+            if (!appComponentsByName.containsKey(annotationName)) {
+                Object[] parameters = getParameters(method);
+                makeBean(method, configClassInstance, annotationName, parameters);
             } else {
                 throw new DiException("method duplicates");
             }
@@ -103,8 +71,49 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         try {
             Object bean = method.invoke(instance, args);
             appComponentsByName.put(name, bean);
+            appComponents.add(bean);
         } catch (IllegalAccessException | InvocationTargetException e) {
             throw new DiException(e.getMessage());
         }
+    }
+
+    private List<Method> getMethods(Class<?> configClass) {
+        List<Method> methods = new ArrayList<>();
+        for (Method method : configClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(AppComponent.class)) {
+                methods.add(method);
+            }
+        }
+
+        methods.sort((Comparator.comparingInt(
+                o -> o.getDeclaredAnnotation(AppComponent.class).order())));
+        return methods;
+    }
+
+    private Object getInstance(Class<?> configClass) {
+        try {
+            Constructor<?> constructor = configClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (NoSuchMethodException
+                | InvocationTargetException
+                | InstantiationException
+                | IllegalAccessException e) {
+            throw new DiException(e.getMessage());
+        }
+    }
+
+    private Object[] getParameters(Method method) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+
+        if (parameterTypes.length == 0) {
+            return parameterTypes;
+        }
+
+        Object[] parameters = new Object[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++) {
+            parameters[i] = getAppComponent(parameterTypes[i]);
+        }
+        return parameters;
     }
 }
