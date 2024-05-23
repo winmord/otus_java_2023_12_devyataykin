88b1ff91a426fe7d8cc2856477416d29f51c4c4e
winmord <winmord@yandex.ru>
2024-05-24 01:29:37 +0300

    feature: implement homework task

---
+++ hw16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -0,0 +1,59 @@
+package ru.otus.services.processors;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.api.SensorDataProcessor;
+import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
+
+// Этот класс нужно реализовать
+@SuppressWarnings({"java:S1068", "java:S125"})
+public class SensorDataProcessorBuffered implements SensorDataProcessor {
+    private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
+
+    private final int bufferSize;
+    private final SensorDataBufferedWriter writer;
+    private final BlockingQueue<SensorData> dataBuffer;
+
+    public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
+        this.bufferSize = bufferSize;
+        this.writer = writer;
+        dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
+    }
+
+    @Override
+    public void process(SensorData data) {
+        try {
+            dataBuffer.put(data);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
+        }
+    }
+
+    public void flush() {
+        try {
+            List<SensorData> bufferedData = new ArrayList<>();
+            dataBuffer.drainTo(bufferedData);
+
+            if (!bufferedData.isEmpty()) {
+                writer.writeBufferedData(bufferedData);
+            }
+        } catch (Exception e) {
+            log.error("Ошибка в процессе записи буфера", e);
+        }
+    }
+
+    @Override
+    public void onProcessingEnd() {
+        flush();
+    }
+}
