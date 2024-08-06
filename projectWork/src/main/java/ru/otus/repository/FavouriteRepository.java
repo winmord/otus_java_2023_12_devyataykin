99302276a574994d44232a86128cb14803403986
winmord <winmord@yandex.ru>
2024-08-06 16:18:20 +0300

    refactor: return List interface instead of Collection in repository

--- projectWork/src/main/java/ru/otus/repository/FavouriteRepository.java
+++ projectWork/src/main/java/ru/otus/repository/FavouriteRepository.java
@@ -1,14 +1,15 @@
 package ru.otus.repository;
 
-import java.util.Collection;
-import java.util.Optional;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.otus.model.Favourite;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavouriteRepository extends ListCrudRepository<Favourite, Long> {
     Optional<Favourite> findByChatIdAndFilmId(Long chatId, String filmId);
 
-    Collection<Favourite> findAllByChatId(Long chatId);
+    List<Favourite> findAllByChatId(Long chatId);
 }
